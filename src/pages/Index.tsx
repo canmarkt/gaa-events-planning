f8d1e21eb23a986fd8b3cca0d237b0d657166ea1
canmarkt <86751326+canmarkt@users.noreply.github.com>
2025-05-30 00:22:34 -0700

    Copyright Updated

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -238,7 +238,7 @@ const Index = () => {
           </div>
           
           <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-            <p>&copy; 2025 GAA WeddingPro. All rights reserved.</p>
+            <p>&copy; 2025 GAA Events. All rights reserved.</p>
           </div>
         </div>
       </footer>
